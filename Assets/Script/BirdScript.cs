8d88c6645e4bf38f76b3cd4f64f46ee881fa5a8d
Deroxon <55814631+Deroxon@users.noreply.github.com>
2023-12-19 17:02:16 +0100

    Logic
    Jumping
    Adjusted gravity scale, move speed
    pipes spawner
    pipe prefab

---
+++ Assets/Script/BirdScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+    public Rigidbody2D rigidbody2D;
+
+    public float flapStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if(Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            rigidbody2D.velocity = Vector2.up * flapStrength;
+        }
+
+
+    }
+}
