62dd7049276ee65dc703d17f27be12e066739fc1
Deroxon <55814631+Deroxon@users.noreply.github.com>
2023-12-26 06:31:16 +0100

    MUSIC

---
+++ Assets/Script/MusicScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    public Sprite speaker, Unspeaker;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("głośnik");
+        if (audioSource.isPlaying)
+        {
+            audioSource.Pause();
+            spriteRenderer.sprite = Unspeaker;
+        }
+        else
+        {
+            audioSource.Play();
+            spriteRenderer.sprite = speaker;
+        }
+    }
+
+}
